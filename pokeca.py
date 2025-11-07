2d9bd335de5c323df2feac7f324b2c76d0a9b7e7
main <jjump88@gmail.com>
2025-11-07 14:00:54 +0000

    add html to the table

--- pokeca.py
+++ pokeca.py
@@ -7,7 +7,7 @@ import threading
 
 cards = []
 
-csv_file = "cards.csv"
+csv_file = "cards_test.csv"
 
 # overwrite file
 df = pd.DataFrame(columns=["id", "name", "price"])
@@ -23,36 +23,39 @@ def request_card_price(id):
     # card id
     base_url_2 = "https://pokeca-chart.com/ch/php/get-item-btn-link.php"
 
-    info_res = requests.get(base_url_2, params={"item_id": id}, timeout=10)
-    info_data = info_res.json()
-    if not info_data:
-        return None, None
-
     price_res = requests.get(
         base_url_1,
         params={"function": "get_shop_stock_data", "item_id": id},
         timeout=10,
     )
+
+    info_res = requests.get(base_url_2, params={"item_id": id}, timeout=10)
+    info_data = info_res.json()
+    if not info_data:
+        return None, None, None
+
+  
     price_data = price_res.json()
 
     if not price_data:
-        return None, None
+        return None, None, None
 
     snkrdunk_min_price = None
 
     # loop for price data from snkrdunk
     for i in price_data:
         if i["shop_id"] == 9 and i["item_status"] == 2:
-            snkrdunk_min_price = i["min_price"]
+            snkrdunk_min_price = i["min_price"] 
+            snkrdunk_shop_link = i["url"]
 
-    if snkrdunk_min_price == None:
-        return None, None
+    if snkrdunk_min_price is None:
+        return None, None, None
 
-    return info_data["name"], snkrdunk_min_price
+    return info_data["name"], snkrdunk_min_price, snkrdunk_shop_link
 
 
-def add_card_to_table(df, card_id, card_name, card_price, csv_file):
-    new_row = {"id": card_id, "name": card_name, "price": card_price}
+def add_card_to_table(df, card_id, card_name, card_price, url, csv_file):
+    new_row = {"id": card_id, "name": card_name, "price": card_price, "url": url}
     df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
     df.to_csv(csv_file, index=False)
     return df
@@ -74,7 +77,7 @@ def fetch_batch(start_id, end_id, workers=10):
         for future in as_completed(futures):
             card_id = futures[future]
             try:
-                name, price = future.result()
+                name, price, link = future.result()
             except Exception as e:
                 print(f"id {card_id}: error -> {e}")
                 continue
@@ -84,7 +87,7 @@ def fetch_batch(start_id, end_id, workers=10):
                 # protect concurrent writes to df / CSV
                 with write_lock:
                     name = re.search(r"\[(.*?)\]", name).group(1)
-                    df = add_card_to_table(df, card_id, name, price, csv_file)
+                    df = add_card_to_table(df, card_id, name, price, link, csv_file)
                 print(f"id {card_id}: added ({name}, {price} JPY)")
             else:
                 none_streak += 1
